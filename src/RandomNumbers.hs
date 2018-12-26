e7f2af583972d4954c7619fb6bf8919aa041a2f8
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-12-26 21:55:24 +0500

    add rand strings

--- src/RandomNumbers.hs
+++ src/RandomNumbers.hs
@@ -12,3 +12,15 @@ fiveRands = [first, second, third, fourth, fifth]
     (third, thirdSeed) = rand sndSeed
     (fourth, fourthSeed) = rand thirdSeed
     (fifth, _) = rand fourthSeed
+
+randLetter :: Seed -> (Char, Seed)
+randLetter seed = (toLetter number, newSeed)
+  where
+    (number, newSeed) = rand seed
+
+randString3 :: String
+randString3 = [first, second, third]
+  where
+    (first, fstSeed) = randLetter $ mkSeed 1
+    (second, sndSeed) = randLetter fstSeed
+    (third, _) = randLetter sndSeed
