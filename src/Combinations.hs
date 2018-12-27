9cd4a3ce306ee112c43d5236ac45fa89fdbedd22
dskurikhin <d.skurikhin@eastwind.ru>
2018-12-27 16:47:36 +0500

    add some combinations

---
+++ src/Combinations.hs
@@ -0,0 +1,33 @@
+module Combinations where
+
+import Prelude (undefined)
+import MCPrelude
+
+allPairs :: [a] -> [b] -> [(a,b)]
+allPairs [] _ = []
+allPairs _ [] = []
+allPairs (x:xs) ys = map ((,) x) ys ++ allPairs xs ys
+
+data Card = Card Int String
+
+instance Show Card where
+  show (Card x y) = show x ++ y
+
+allCards :: [Int] -> [String] -> [Card]
+allCards [] _ = []
+allCards _ [] = []
+allCards (x:xs) ys = map (Card x) ys ++ allCards xs ys
+
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs _ [] _ = []
+allCombs _ _ [] = []
+allCombs f (x:xs) ys = map (f x) ys ++ allCombs f xs ys
+
+allPairs' = allCombs (,)
+allCards' = allCombs Card
+
+allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3 _ [] _ _ = []
+allCombs3 _ _ [] _ = []
+allCombs3 _ _ _ [] = []
+allCombs3 f (x:xs) ys zs = concatMap (\z -> map (\y -> f x y z) ys) zs ++ allCombs3 f xs ys zs
