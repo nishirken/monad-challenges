63e22592390f5c20de964d1210038c3208798ef8
dskurikhin <d.skurikhin@eastwind.ru>
2018-12-27 17:27:42 +0500

    complete combinations

--- src/Combinations.hs
+++ src/Combinations.hs
@@ -31,3 +31,14 @@ allCombs3 _ [] _ _ = []
 allCombs3 _ _ [] _ = []
 allCombs3 _ _ _ [] = []
 allCombs3 f (x:xs) ys zs = concatMap (\z -> map (\y -> f x y z) ys) zs ++ allCombs3 f xs ys zs
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep [] _ = []
+combStep _ [] = []
+combStep (f:fs) xs = map f xs ++ combStep fs xs
+
+allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs' f xs = combStep $ map f xs
+
+allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3' f xs ys = combStep (combStep (map f xs) ys)
